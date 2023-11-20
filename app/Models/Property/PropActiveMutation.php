c90fcdfd744dbcc1ba1c6ff04bd2a614390b2467
sandeepBara <sandeepbara830@gmail.com>
2023-11-20 18:12:13 +0530

    dhfhdfhg

---
+++ app/Models/Property/PropActiveMutation.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Property;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PropActiveMutation extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+}
