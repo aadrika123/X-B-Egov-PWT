9b1b7e80dbde449ed9f846c12dcedd069b00b472
sandeepBara <sandeepbara830@gmail.com>
2023-12-07 16:05:06 +0530

    sdff afsd

--- app/Http/Controllers/Property/SafDocController.php
+++ app/Http/Controllers/Property/SafDocController.php
@@ -393,8 +393,8 @@ class SafDocController extends Controller
                 throw new Exception("Saf Details Not Found");
 
             $ifFullDocVerified = $this->ifFullDocVerified($applicationId);       // (Current Object Derivative Function 4.1)
-            if ($ifFullDocVerified == 1)
-                throw new Exception("Document Fully Verified");
+            // if ($ifFullDocVerified == 1)
+            //     throw new Exception("Document Fully Verified");
 
             DB::beginTransaction();
             if ($req->docStatus == "Verified") {
