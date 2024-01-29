3addd8817d669563f4a66006ae19881481a83c80
rohan8883 <rohansingh9135@gmail.com>
2024-01-29 18:59:38 +0530

    fsafd as

--- app/BLL/Property/Akola/GeneratePaymentReceiptV2.php
+++ app/BLL/Property/Akola/GeneratePaymentReceiptV2.php
@@ -328,7 +328,7 @@ class GeneratePaymentReceiptV2
         }
         if($from !=$to-1)
         {
-            $duration = "01-April-".($from) ." to "."31-March-".($to-1)." and April-01-".($to-1)." to "."March-31-".($to);
+            $duration = "01-April-".($from) ." to "."31-March-".($to-1)." and 01-April-".($to-1)." to "."31-March-".($to);
         }
         // dd($from,$to,$duration);
         $receiptDtls = [
