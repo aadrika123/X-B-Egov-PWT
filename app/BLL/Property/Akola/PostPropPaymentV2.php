4cf1ba542ef1c4d7a4786766bacad8793d01f619
rohan8883 <rohansingh9135@gmail.com>
2024-03-18 18:20:31 +0530

    OK

--- app/BLL/Property/Akola/PostPropPaymentV2.php
+++ app/BLL/Property/Akola/PostPropPaymentV2.php
@@ -834,7 +834,7 @@ class PostPropPaymentV2
         $this->_fromFyear = ((collect($paidDemands)->sortBy("fyear"))->first())["fyear"] ?? $this->_fromFyear;
         $this->_uptoFyear = ((collect($paidDemands)->sortBy("fyear"))->last())["fyear"] ?? $this->_uptoFyear;
 
-        $propTrans = $this->_mPropTrans->postPropTransactions($this->_REQ, $paidDemands, $this->_fromFyear, $this->_uptoFyear);dd($propTrans);
+        $propTrans = $this->_mPropTrans->postPropTransactions($this->_REQ, $paidDemands, $this->_fromFyear, $this->_uptoFyear);
         $this->_tranId = $propTrans['id'];
         $this->_propTransaction = $propTrans;
         $this->_penaltyRebates["monthlyPenalty"]["amount"] = roundFigure($paidPenalty);
