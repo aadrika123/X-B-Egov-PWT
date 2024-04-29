8cf4cbaf88389c9aacc7847001851b4a50488f81
rohan8883 <rohansingh9135@gmail.com>
2024-04-29 10:18:59 +0530

    activate monthely rebate

--- config/akola-property-constant.php
+++ config/akola-property-constant.php
@@ -39,12 +39,12 @@ return [
         "2019-2020"=>0.4,
         "2018-2019"=>0.2,
     ],
-    "FIRST_QUIETER_REBATE1"=>[
+    "FIRST_QUIETER_REBATE"=>[
         "0"=>[
             "rebate_type"=>"First Quieter Rebate",
             "effective_from"=>"2024-04-01",
             "from_date"=>Carbon::parse(Carbon::now()->format("Y")."-04-01")->format("Y-m-d"),
-            "upto_date"=>Carbon::parse(Carbon::now()->format("Y")."-05-14")->format("Y-m-d"),
+            "upto_date"=>Carbon::parse(Carbon::now()->format("Y")."-05-15")->format("Y-m-d"),
             "rebates"=>7,
             "rebates_in_perc"=>true,
             "apply_on_current_tax"=>true,
@@ -52,8 +52,8 @@ return [
         "1"=>[
             "rebate_type"=>"First Quieter Rebate",
             "effective_from"=>"2024-04-01",
-            "from_date"=>Carbon::parse(Carbon::now()->format("Y")."-05-15")->format("Y-m-d"),
-            "upto_date"=>Carbon::parse(Carbon::now()->format("Y")."-06-14")->format("Y-m-d"),
+            "from_date"=>Carbon::parse(Carbon::now()->format("Y")."-05-16")->format("Y-m-d"),
+            "upto_date"=>Carbon::parse(Carbon::now()->format("Y")."-06-15")->format("Y-m-d"),
             "rebates"=>6,
             "rebates_in_perc"=>true,
             "apply_on_current_tax"=>true,
@@ -61,8 +61,8 @@ return [
         "2"=>[
             "rebate_type"=>"First Quieter Rebate",
             "effective_from"=>"2024-04-01",
-            "from_date"=>Carbon::parse(Carbon::now()->format("Y")."-06-15")->format("Y-m-d"),
-            "upto_date"=>Carbon::parse(Carbon::now()->format("Y")."-07-14")->format("Y-m-d"),
+            "from_date"=>Carbon::parse(Carbon::now()->format("Y")."-06-16")->format("Y-m-d"),
+            "upto_date"=>Carbon::parse(Carbon::now()->format("Y")."-07-15")->format("Y-m-d"),
             "rebates"=>5,            
             "rebates_in_perc"=>true,
             "apply_on_current_tax"=>true,
