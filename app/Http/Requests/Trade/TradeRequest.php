3f1da07b2b534e3f9ea2cb9b93c9e47130e72f17
pandeyprity <pandeyprity33@gmail.com>
2024-07-25 13:41:05 +0530

    corrected code

--- app/Http/Requests/Trade/TradeRequest.php
+++ app/Http/Requests/Trade/TradeRequest.php
@@ -58,7 +58,8 @@ class TradeRequest extends FormRequest
         $this->_REX_ALPHA_NUM                               = "/^[a-zA-Z0-9-]+$/i";
         $this->_REX_ALPHA_NUM_SPACE                         = "/^[a-zA-Z0-9- ]+$/i";
         $this->_REX_ALPHA_NUM_DOT_SPACE                     = "/^[a-zA-Z0-9][a-zA-Z0-9\. \s]+$/i";
-        $this->_REX_ALPHA_NUM_OPS_DOT_MIN_COM_AND_SPACE_SL  = "/^[a-zA-Z0-9][a-zA-Z0-9\'\.\-\,\&\s\/]+$/i";
+        //$this->_REX_ALPHA_NUM_OPS_DOT_MIN_COM_AND_SPACE_SL  = "/^[a-zA-Z0-9][a-zA-Z0-9\'\.\-\,\&\s\/]+$/i";
+        $this->_REX_ALPHA_NUM_OPS_DOT_MIN_COM_AND_SPACE_SL  = "/^[\p{L}\p{N}\s\.\,\(\)\'\p{Devanagari}]*$/u";
         $this->_REX_NUM_DOT                                 = "/^\d+(?:\.\d+)+$/i";
         $this->_REX_OWNER_NAME = "/^[\p{L}\p{N}\s\.\,\(\)\'\p{Devanagari}]*$/u";
 
