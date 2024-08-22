44a4d2993ed405bf9f167cf4cab2c9d4480c405b
mrinal9911 <mrinalk123456@gmail.com>
2024-08-22 19:06:10 +0530

    changes

--- app/Http/Controllers/Water/NewConnectionController.php
+++ app/Http/Controllers/Water/NewConnectionController.php
@@ -1836,7 +1836,7 @@ class NewConnectionController extends Controller
         $applicationId      = $refWaterApplication->id;
         $workflowId         = $refWaterApplication->workflow_id;
         $moduleId           = Config::get('module-constants.WATER_MODULE_ID');
-        $uploadedDocs       = $mWfActiveDocument->getDocByRefIds($applicationId, $workflowId, $moduleId);
+        $uploadedDocs       = $mWfActiveDocument->getDocByRefIdsV4($applicationId, $workflowId, $moduleId);
 
         $explodeDocs = collect(explode('#', $documentList->requirements));
         $filteredDocs = $explodeDocs->map(function ($explodeDoc) use ($uploadedDocs, $ownerId, $documentList) {
