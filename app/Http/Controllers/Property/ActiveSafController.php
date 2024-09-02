a947adb9e9eafdcb862af2dd0da54da76e9ea366
pandeyprity <pandeyprity33@gmail.com>
2024-09-02 12:58:32 +0530

    saf code corrition for pending level

--- app/Http/Controllers/Property/ActiveSafController.php
+++ app/Http/Controllers/Property/ActiveSafController.php
@@ -937,6 +937,7 @@ class ActiveSafController extends Controller
             // Derivative Assignments
             $data = $mPropActiveSaf->getActiveSafDtls()                         // <------- Model function Active SAF Details
                 ->where('prop_active_safs.id', $req->applicationId)
+                ->where('citizen_id',null)
                 ->first();
             // if (!$data)
             // throw new Exception("Application Not Found");
