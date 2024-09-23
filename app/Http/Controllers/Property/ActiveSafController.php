cbdcf23f3779eb4dd453793eeda1eee91b283dbf
pandeyprity <pandeyprity33@gmail.com>
2024-09-23 18:09:21 +0530

    corrected code for tc workflow

--- app/Http/Controllers/Property/ActiveSafController.php
+++ app/Http/Controllers/Property/ActiveSafController.php
@@ -1554,7 +1554,10 @@ class ActiveSafController extends Controller
                     $forwardBackwardIds->forward_role_id = $wfLevels['DA'];
                 }
                 if (!$geotagExist && $saf->current_role == $wfLevels['DA'] && !in_array($wfMstrId, $this->_SkipFiledWorkWfMstrId)) {
-                    $forwardBackwardIds->forward_role_id = $wfLevels['UTC'];
+                    //$forwardBackwardIds->forward_role_id = $wfLevels['UTC'];
+
+                    //change by prity pandey
+                    $forwardBackwardIds->forward_role_id = $wfLevels['TC'];
                     if ($saf->prop_type_mstr_id == 4) {
                         $forwardBackwardIds->forward_role_id = $wfLevels['TC'];
                     }
