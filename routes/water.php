72c46a5d4acba1de6b92ed8e7e7249c414946151
mrinal9911 <mrinalk123456@gmail.com>
2024-09-24 17:36:51 +0530

    changes

--- routes/water.php
+++ routes/water.php
@@ -256,7 +256,7 @@ Route::group(['middleware' => ['json.response', 'auth_maker']], function () { //
         Route::post('report/ward-wise-demand', 'wardWiseConsumerReport');
         Route::post('report/billing-summary', 'billingSummary');
         // Route::post('bill/demand-bill', 'waterBulkdemand');                                                   // demand bulk view bill
-        Route::post('bill/demand-bill', 'waterBulkdemandV2');
+        Route::post('bill/demand-bill', 'waterBulkdemandV7');
         Route::post('consumer/demand-bill', 'waterBulkdemandV3');
         Route::post('receipt/bulk-receipt', 'bulkReceipt');
         Route::post('report/date-wise-collection', 'dateCollectuionReport');
