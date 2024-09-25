6a2c5eba7566393a7622b11b7f6d5bec1cb18c7c
mrinal9911 <mrinalk123456@gmail.com>
2024-09-25 13:28:47 +0530

    changes

--- app/Http/Controllers/Water/WaterReportController.php
+++ app/Http/Controllers/Water/WaterReportController.php
@@ -3114,9 +3114,6 @@ class WaterReportController extends Controller
                     wd.status = TRUE 
                     AND wd.consumer_id IS NOT NULL 
                     AND wd.due_balance_amount>0 
-
-                     " . ($fromDate ? " AND wd.demand_from >= '$fromDate'" : "") . "
-                    " . ($uptoDate ? " AND wd.demand_upto <= '$uptoDate'" : "") . "
                     GROUP BY 
                     wd.consumer_id 
                 ),
@@ -3220,7 +3217,6 @@ class WaterReportController extends Controller
                 WHERE  1=1
                 " . ($wardId ? " AND water_second_consumers.ward_mstr_id = $wardId" : "") . "    
                 " . ($zoneId ? " AND water_second_consumers.zone_mstr_id = $zoneId" : "") . "  
-             
                 " . ($metertype ? ($metertype == 3
                 ? " AND (connection_types.current_meter_status IN($metertype) OR connection_types.consumer_id IS NULL )"
                 : " AND connection_types.current_meter_status IN($metertype)"
