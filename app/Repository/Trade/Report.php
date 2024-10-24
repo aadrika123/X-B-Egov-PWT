df2ee6cdabd662849b48904b3d4c589169efe78e
pandeyprity <pandeyprity33@gmail.com>
2024-10-24 18:07:52 +0530

    error sql

--- app/Repository/Trade/Report.php
+++ app/Repository/Trade/Report.php
@@ -2211,7 +2211,7 @@ class Report implements IReport
                         TO_CHAR(CAST(licences.valid_upto AS DATE), 'DD-MM-YYYY') as valid_upto,
                         CASE 
                     CASE 
-                    WHEN licences.is_active = false OR licences.reject_remarks IS NOT NULL THEN 'inactive'
+                    WHEN licences.is_active = false THEN 'inactive'
                     WHEN licences.is_active = true THEN 'active'
                     ELSE 'unknown'
                 END as status "),
